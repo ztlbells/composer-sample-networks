0325f6995c7f7f516aa38d8d79f3b7644d8f953a
ztlbells <zutianluo@gmail.com>
2017-05-20 22:29:05 -0700

    modify txn process funcs in shipment network

--- packages/animaltracking-network/lib/mozart.cto.js
+++ packages/animaltracking-network/lib/mozart.cto.js
@@ -1,238 +1,70 @@
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 'use strict';
 
-/*eslint-disable no-unused-vars*/
-/*eslint-disable no-undef*/
-
-/**
- *
- * @param {com.biz.AnimalMovementDeparture} movementDeparture
- * @transaction
- */
-function onAnimalMovementDeparture(movementDeparture) {
-  console.log('onAnimalMovementDeparture');
-  if (movementDeparture.animal.movementStatus !== 'IN_FIELD') {
-    throw new Error('Animal is already IN_TRANSIT');
+function onShipmentDeparture(movementDeparture) {
+  console.log('onShipmentDeparture');
+  if (movementDeparture.animal.movementStatus !== 'IN_STATION') {
+    throw new Error('Shipment is already IN_TRANSIT');
   }
 
-     // set the movement status of the animal
-  movementDeparture.animal.movementStatus = 'IN_TRANSIT';
+     // set the status of the shipment
+  movementDeparture.shipment.shipmentStatus = 'IN_TRANSIT';
 
-     // save the animal
-  return getAssetRegistry('com.biz.Animal')
+     // save the shipment
+  return getAssetRegistry('com.biz.Shipment')
   .then(function(ar) {
-    return ar.update(movementDeparture.animal);
+    return ar.update(movementDeparture.shipment);
   })
   .then(function() {
-    // add the animal to the incoming animals of the
-    // destination business
-    if (movementDeparture.to.incomingAnimals) {
-      movementDeparture.to.incomingAnimals.push(movementDeparture.animal);
+    // add the shipment to the incoming shipments of the
+    // destination facility
+    if (movementDeparture.to.incomingShipments) {
+      movementDeparture.to.incomingShipments.push(movementDeparture.shipment);
     } else {
-      movementDeparture.to.incomingAnimals = [movementDeparture.animal];
+      movementDeparture.to.incomingShipments = [movementDeparture.shipment];
     }
     
-      // save the business
-     return getAssetRegistry('com.biz.Business')
+      // save the facility
+     return getAssetRegistry('com.biz.Facility')
   })
   .then(function(br) {
     return br.update(movementDeparture.to);
   });
 }
 
-/**
- *
- * @param {com.biz.AnimalMovementArrival} movementArrival
- * @transaction
- */
-function onAnimalMovementArrival(movementArrival) {
+function onShipmentArrival(movementArrival) {
   console.log('onAnimalMovementArrival');
 
-  if (movementArrival.animal.movementStatus !== 'IN_TRANSIT') {
-    throw new Error('Animal is not IN_TRANSIT');
+  if (movementDeparture.animal.movementStatus !== 'IN_TRANSIT') {
+    throw new Error('Shipment is not IN_TRANSIT');
   }
 
-     // set the movement status of the animal
-  movementArrival.animal.movementStatus = 'IN_FIELD';
+  movementDeparture.shipment.shipmentStatus = 'IN_FIELD';
 
-     // set the new owner of the animal
-     // to the owner of the 'to' business
-  movementArrival.animal.owner = movementArrival.to.owner;
+     // set the new PIC of the station where the shipment arrives
+  movementArrival.shipment.person_in_charge = movementArrival.to.person_in_charge;
 
-     // set the new location of the animal
-  movementArrival.animal.location = movementArrival.arrivalField;
+     // set the new location (facility) of the shipment 
+  movementArrival.animal.location = movementArrival.arrivalFacility;
 
-     // save the animal
-  return getAssetRegistry('com.biz.Animal')
+  return getAssetRegistry('com.biz.Shipment')
   .then(function(ar) {
-    return ar.update(movementArrival.animal);
+    return ar.update(movementArrival.shipment);
   })
   .then(function() {
-    // remove the animal from the incoming animals
-    // of the 'to' business
-    if (!movementArrival.to.incomingAnimals) {
-      throw new Error('Incoming business should have incomingAnimals on AnimalMovementArrival.');
+    // remove the shipment from the incoming shipment of the 'to' facility
+    if (!movementArrival.to.incomingShipments) {
+      throw new Error('The facility should have incoming shipments.');
     }
 
-    movementArrival.to.incomingAnimals = movementArrival.to.incomingAnimals
-    .filter(function(animal) {
-      return animal.animalId !== movementArrival.animal.animalId;
+    movementArrival.to.incomingShipments = movementArrival.to.incomingShipments
+    .filter(function(shipment) {
+      return shipment.trackingId !== movementArrival.shipment.trackingId;
     });
 
-      // save the business
-    return getAssetRegistry('com.biz.Business');
+      // save the Facility
+    return getAssetRegistry('com.biz.Facility');
   })
   .then(function(br) {
     return br.update(movementArrival.to);
   });
 }
-
-/**
- *
- * @param {com.biz.SetupDemo} setupDemo
- * @transaction
- */
-function setupDemo(setupDemo) {
-  var factory = getFactory();
-  var NS = 'com.biz';
-
-  var farmers = [
-    factory.newResource(NS, 'Farmer', 'FARMER_1'),
-    factory.newResource(NS, 'Farmer', 'FARMER_2')
-  ];
-
-  var businesses = [
-    factory.newResource(NS, 'Business', 'BUSINESS_1'),
-    factory.newResource(NS, 'Business', 'BUSINESS_2')
-  ];
-
-  var fields = [
-    factory.newResource(NS, 'Field','FIELD_1'),
-    factory.newResource(NS, 'Field','FIELD_2'),
-    factory.newResource(NS, 'Field','FIELD_3'),
-    factory.newResource(NS, 'Field','FIELD_4')
-  ];
-
-  var animals = [
-    factory.newResource(NS, 'Animal', 'ANIMAL_1'),
-    factory.newResource(NS, 'Animal', 'ANIMAL_2'),
-    factory.newResource(NS, 'Animal', 'ANIMAL_3'),
-    factory.newResource(NS, 'Animal', 'ANIMAL_4'),
-    factory.newResource(NS, 'Animal', 'ANIMAL_5'),
-    factory.newResource(NS, 'Animal', 'ANIMAL_6'),
-    factory.newResource(NS, 'Animal', 'ANIMAL_7'),
-    factory.newResource(NS, 'Animal', 'ANIMAL_8')
-  ];
-  return getParticipantRegistry(NS + '.Regulator')
-  .then(function(regulatorRegistry) {
-    var regulator = factory.newResource(NS, 'Regulator', 'REGULATOR');
-    regulator.email = 'REGULATOR';
-    regulator.firstName = 'Ronnie';
-    regulator.lastName = 'Regulator';
-    return regulatorRegistry.addAll([regulator]);
-  })
-  .then(function() {
-    return getParticipantRegistry(NS + '.Farmer');
-  })
-  .then(function(farmerRegistry) {
-    farmers.forEach(function(farmer) {
-      var sbi = 'BUSINESS_' + farmer.getIdentifier().split('_')[1];
-      farmer.firstName = farmer.getIdentifier();
-      farmer.lastName = '';
-      farmer.address1 = 'Address1';
-      farmer.address2 = 'Address2';
-      farmer.county = 'County';
-      farmer.postcode = 'PO57C0D3';
-      farmer.business = factory.newResource(NS, 'Business', sbi);
-    });
-    return farmerRegistry.addAll(farmers);
-  })
-  .then(function() {
-    return getAssetRegistry(NS + '.Business');
-  })
-  .then(function(businessRegistry) {
-    businesses.forEach(function(business, index) {
-      var cph = 'FIELD_' + (index + 1);
-      var farmer = 'FARMER_' + (index + 1);
-      business.address1 = 'Address1';
-      business.address2 = 'Address2';
-      business.county = 'County';
-      business.postcode = 'PO57C0D3';
-      business.owner = factory.newRelationship(NS, 'Farmer', farmer);
-    });
-
-    return businessRegistry.addAll(businesses);
-  })
-  .then(function() {
-    return getAssetRegistry(NS + '.Field');
-  })
-  .then(function(fieldRegistry) {
-    fields.forEach(function(field, index) {
-      var business = 'BUSINESS_' + (((index + 1) % 2) + 1);
-      field.name = 'FIELD_' + (index + 1);
-      field.business = factory.newRelationship(NS, 'Business', business);
-    });
-    return fieldRegistry.addAll(fields);
-  })
-  .then(function() {
-    return getAssetRegistry(NS + '.Animal');
-  })
-  .then(function(animalRegistry) {
-    animals.forEach(function(animal, index) {
-      var field = 'FIELD_' + (((index + 1) % 2) + 1);
-      var farmer = 'FARMER_' + (((index + 1) % 2) + 1);
-      animal.species = 'SHEEP_GOAT';
-      animal.movementStatus = 'IN_FIELD';
-      animal.productionType = 'MEAT';
-      animal.location = factory.newRelationship(NS, 'Field', field);
-      animal.owner = factory.newRelationship(NS, 'Farmer', farmer);
-    });
-    return animalRegistry.addAll(animals);
-  });
-} 
-
-/**
- * Get the Animals, but do not resolve contained relationships
- * @query
- * @param {String} farmerId - the email of the farmer
- * @returns {Animal[]} - the animals that belong to the farmer
-*/
-function findAnimalsByOwnerId(farmerId) {
-  return query('select a from Animal a where a.owner == :farmerId');
-}
-
-/**
- * Get the Animals, but and selectively resolve relationships
- * @query
- * @param {String} farmerId - the email of the farmer
- * @returns {Animal[]} - the animals that belong to the farmer
-*/
-function findAnimalsByOwnerIdWithDetails(farmerId) {
-  return query('select resolve(a, a.location, a.owner) from Animal a where a.owner == :farmerId');
-}
-
-/**
- * Get the incoming animals for a farmer and do not resolve relationships
- * @query
- * @param {String} farmerId - the email of the farmer
- * @returns {Animal[]} - the animals that belong to the farmer
-*/
-function findIncomingAnimalsByFarmerId(farmerId) {
-  return query('select b.incomingAnimals from Business b where b.owner == :farmerId');
-}
-
-/*eslint-enable no-unused-vars*/
-/*eslint-enable no-undef*/
